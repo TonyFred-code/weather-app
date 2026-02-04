95ebb36cb9a68b2afa8299ebf6e1dcea6047f647
Alfred Faith <111481054+TonyFred-code@users.noreply.github.com>
2026-02-04 14:31:17 +0100

    feat: add content for daily weather forecast

---
+++ src/components/DailyForecast.jsx
@@ -0,0 +1,85 @@
+export default function DailyForecast() {
+  return (
+    <section className="lg:col-span-2 row-start-2 space-y-3.5">
+      <h2 className="text-lg">Daily forecast</h2>
+      <div className="*:rounded-lg *:flex *:flex-col *:items-center *:bg-neutral-700 grid grid-cols-3 md:grid-cols-7 gap-3 *:p-2 *:gap-3">
+        <div>
+          <p className="capitalize text-sm">tue</p>
+          <p className="size-12">
+            <img src="/images/icon-rain.webp" alt="Rainy weather" />
+          </p>
+          <p className="flex justify-between self-stretch text-xs">
+            <span>20°</span>
+            <span>14°</span>
+          </p>
+        </div>
+
+        <div>
+          <p className="capitalize text-sm">tue</p>
+          <p className="size-12">
+            <img src="/images/icon-rain.webp" alt="Rainy weather" />
+          </p>
+          <p className="flex justify-between self-stretch text-xs">
+            <span>20°</span>
+            <span>14°</span>
+          </p>
+        </div>
+
+        <div>
+          <p className="capitalize text-sm">tue</p>
+          <p className="size-12">
+            <img src="/images/icon-rain.webp" alt="Rainy weather" />
+          </p>
+          <p className="flex justify-between self-stretch text-xs">
+            <span>20°</span>
+            <span>14°</span>
+          </p>
+        </div>
+
+        <div>
+          <p className="capitalize text-sm">tue</p>
+          <p className="size-12">
+            <img src="/images/icon-rain.webp" alt="Rainy weather" />
+          </p>
+          <p className="flex justify-between self-stretch text-xs">
+            <span>20°</span>
+            <span>14°</span>
+          </p>
+        </div>
+
+        <div>
+          <p className="capitalize text-sm">tue</p>
+          <p className="size-12">
+            <img src="/images/icon-rain.webp" alt="Rainy weather" />
+          </p>
+          <p className="flex justify-between self-stretch text-xs">
+            <span>20°</span>
+            <span>14°</span>
+          </p>
+        </div>
+
+        <div>
+          <p className="capitalize text-sm">tue</p>
+          <p className="size-12">
+            <img src="/images/icon-rain.webp" alt="Rainy weather" />
+          </p>
+          <p className="flex justify-between self-stretch text-xs">
+            <span>20°</span>
+            <span>14°</span>
+          </p>
+        </div>
+
+        <div>
+          <p className="capitalize text-sm">tue</p>
+          <p className="size-12">
+            <img src="/images/icon-rain.webp" alt="Rainy weather" />
+          </p>
+          <p className="flex justify-between self-stretch text-xs">
+            <span>20°</span>
+            <span>14°</span>
+          </p>
+        </div>
+      </div>
+    </section>
+  );
+}
