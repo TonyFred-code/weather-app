001089e2c9a915d82c9616c38f0c05213b8d57e1
Alfred Faith <111481054+TonyFred-code@users.noreply.github.com>
2026-02-04 16:05:19 +0100

    feat: add Header, Form & WeatherOutput components

--- src/App.jsx
+++ src/App.jsx
@@ -1,1 +1,13 @@
-export default function App() {}
+import Form from "./components/Form.jsx";
+import Header from "./components/Header.jsx";
+import WeatherOutput from "./components/WeatherOutput.jsx";
+
+export default function App() {
+  return (
+    <div className="p-4 mx-auto max-w-5xl flex flex-col gap-8 items-center">
+      <Header />
+      <Form />
+      <WeatherOutput />
+    </div>
+  );
+}
