72d9d53488ad0700b3d86409e6e5cb52d40761f8
Alfred Faith <111481054+TonyFred-code@users.noreply.github.com>
2026-02-04 16:02:20 +0100

    feat: create weekDays name constant file
    export name of weekDays to prevent naming inconsistency

---
+++ src/constants/weekDays.js
@@ -0,0 +1,11 @@
+const WEEK_DAYS = {
+  MONDAY: "monday",
+  TUESDAY: "tuesday",
+  WEDNESDAY: "wednesday",
+  THURSDAY: "thursday",
+  FRIDAY: "friday",
+  SATURDAY: "saturday",
+  SUNDAY: "sunday",
+};
+
+export { WEEK_DAYS };
