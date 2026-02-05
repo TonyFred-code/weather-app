90dbacea3487fc5affaded71bff0bedbc3b77afb
Alfred Faith <111481054+TonyFred-code@users.noreply.github.com>
2026-02-05 23:24:33 +0100

    feat: add individual unitsSystem values
    add individual units for each of the unit systems

--- src/constants/unitsSystems.js
+++ src/constants/unitsSystems.js
@@ -3,4 +3,16 @@ const UNIT_SYSTEMS = {
   METRIC: "metric",
 };
 
-export { UNIT_SYSTEMS };
+const METRIC_UNITS = {
+  TEMPERATURE: "celsius",
+  WIND_SPEED: "km/h",
+  PRECIPITATION: "millimeters",
+};
+
+const IMPERIAL_UNITS = {
+  TEMPERATURE: "fahrenheit",
+  WIND_SPEED: "mph",
+  PRECIPITATION: "inches",
+};
+
+export { UNIT_SYSTEMS, IMPERIAL_UNITS, METRIC_UNITS };
