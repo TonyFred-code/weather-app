9eb6c01fd63f8f4f5417a5d0ff5ed3a4aae71a45
Alfred Faith <111481054+TonyFred-code@users.noreply.github.com>
2026-02-06 13:58:30 +0100

    refactor: remove input focus/blur search history

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -9,26 +9,18 @@ export default function Form() {
     }
   }
 
-  function showSearchHistory() {
-    console.log("show search history (last 4)");
-  }
-
-  function hideSearchHistory() {
-    console.log("hide search history drop down");
-  }
-
   return (
     <section className="flex flex-col items-center justify-center">
       <h2 className="text-center my-8 font-bricolage-grotesque text-[60px]">
         How&apos;s the sky looking today?
       </h2>
       <form
-        className="flex flex-col md:flex-row gap-4 md:w-10/12 max-w-xl"
+        className="flex flex-col md:flex-row gap-4 md:w-10/12 max-w-xl w-full"
         onSubmit={handleFormSubmit}
       >
-        <div className="flex gap-4 items-center bg-neutral-800 py-3 px-5 rounded-xl flex-1">
+        <div className="flex xs:gap-2 gap-4 items-center bg-neutral-800 p-3 rounded-xl flex-1">
           <label htmlFor="search">
-            <span>
+            <span className="flex size-3.5">
               <img src="/images/icon-search.svg" alt="" />
             </span>
           </label>
@@ -36,15 +28,14 @@ export default function Form() {
             type="text"
             name="search"
             id="search"
+            required
             placeholder="Search for a place..."
-            className="border-none outline-none flex-1 bg-transparent autofill:bg-transparent autofill:text-neutral-0"
-            onFocus={showSearchHistory}
-            onBlur={hideSearchHistory}
+            className="border-none outline-none flex-1 bg-transparent user-invalid:border-b-2 user-invalid:border-neutral-300 autofill:bg-transparent autofill:text-neutral-0 min-w-0"
           />
         </div>
         <button
           type="submit"
-          className="capitalize text-center text-neutral-0 bg-blue-500 py-2 px-5 md:py-2.5 md:px-6 rounded-xl font-bold cursor-pointer hover:bg-blue-700"
+          className="capitalize text-center text-neutral-0 bg-blue-500 py-3 px-5 md:py-2.5 md:px-6 rounded-xl font-bold cursor-pointer hover:bg-blue-700"
         >
           search
         </button>
