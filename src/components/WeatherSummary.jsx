1c7e9d8eee33779cf78475be4879aa2b5e80b39a
Alfred Faith <111481054+TonyFred-code@users.noreply.github.com>
2026-02-06 13:51:33 +0100

    style: reduce font size on smaller screens

--- src/components/WeatherSummary.jsx
+++ src/components/WeatherSummary.jsx
@@ -1,16 +1,18 @@
 export default function WeatherSummary() {
   return (
     <section className="lg:col-span-2 space-y-8">
-      <div className="bg-blue-500 rounded-2xl bg-[url('/images/bg-today-small.svg')] md:bg-[url('/images/bg-today-large.svg')] bg-no-repeat bg-cover  md:bg-cover bg-center px-8 py-12 flex flex-col md:flex-row">
+      <div className="bg-blue-500 rounded-2xl bg-[url('/images/bg-today-small.svg')] md:bg-[url('/images/bg-today-large.svg')] bg-no-repeat bg-cover bg-center py-12 md:px-8 md:py-14 flex flex-col md:flex-row items-center justify-between">
         <div className="*:text-center">
           <p className="capitalize text-3xl font-bold">berlin, germany</p>
           <p className="text-neutral-0 opacity-70">Tuesday, Aug 5, 2025</p>
         </div>
         <div className="flex items-center justify-around">
-          <span className="size-32">
+          <span className="size-24 md:size-32">
             <img src="/images/icon-sunny.webp" alt="" />
           </span>
-          <span className="text-[90px] font-extrabold italic">20°</span>
+          <span className="text-[72px] md:text-[90px] font-extrabold italic">
+            20°
+          </span>
         </div>
       </div>
       <div className="*:rounded-xl *:bg-neutral-700 *:flex *:flex-col grid grid-cols-2 md:grid-cols-4 *:gap-3 *:flex-1 gap-3 *:p-3">
