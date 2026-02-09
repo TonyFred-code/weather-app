7b0e231a4103d324406ff08336097945892f8566
Alfred Faith <111481054+TonyFred-code@users.noreply.github.com>
2026-02-09 20:47:51 +0100

    feat: create helper function to fetch weather data

---
+++ src/helpers/fetchWeatherData.js
@@ -0,0 +1,30 @@
+import { METRIC_UNITS } from "../constants/unitsSystems.js";
+
+async function fetchWeatherData(longitude, latitude, units) {
+  const temperatureUnitQuery = `${units.TEMPERATURE === METRIC_UNITS.TEMPERATURE ? "" : `&temperature_unit=${units.TEMPERATURE}`}`;
+  const precipitationUnitQuery = `${units.PRECIPITATION === METRIC_UNITS.PRECIPITATION ? "" : `&precipitation_unit=${units.PRECIPITATION}`}`;
+  const windSpeedUnitQuery = `${units.WIND_SPEED === METRIC_UNITS.WIND_SPEED ? "" : `&wind_speed_unit=${units.WIND_SPEED}`}`;
+
+  try {
+    const URL =
+      `https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&daily=temperature_2m_max,temperature_2m_min&hourly=weather_code,apparent_temperature&current=precipitation,relative_humidity_2m,wind_speed_10m,temperature_2m,apparent_temperature&timezone=auto${temperatureUnitQuery}${precipitationUnitQuery}${windSpeedUnitQuery}`.trim();
+
+    const response = await fetch(URL, { mode: "cors" });
+
+    if (!response.ok) {
+      throw new Error("Failed to fetch weather data");
+    }
+
+    const data = await response.json();
+
+    if (!data) {
+      throw new Error("Failed to retrieve weather data");
+    }
+
+    return data;
+  } catch (error) {
+    console.error("Weather data fetching failed with error: ", error);
+  }
+}
+
+export { fetchWeatherData };
